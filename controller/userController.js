a71ca964fa7c551b7c93c0bbf6b68e63f8c289fb
Nazaire02 <nazairendouffou@gmail.com>
2025-01-24 11:22:42 +0000

    login controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,26 @@
 import User from "../models/user.js";
 import bcrypt from 'bcrypt';
-const saltRounds = 10;
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export async function register(req, res){
-    const body = req.body;
+export async function register(req, res) {
     try {
+        const saltRounds = 10;
+        const { name, email, password, role } = req.body;
+        const user = await User.findOne({ email })
+        
+        if (user) {
+            return res.status(409).json({
+                message: "Email already exists"
+            })
+        }
+
         let newUser = new User({
-            name: body.name,
-            email: body.email,
-            password: bcrypt.hashSync(body.password, saltRounds),
-            role: body.role
+            name,
+            email,
+            password: bcrypt.hashSync(password, saltRounds),
+            role
         });
         newUser = await newUser.save();
         return res.status(201).json({
@@ -17,10 +28,40 @@ export async function register(req, res){
             newUser
         })
     } catch (error) {
-        return res.status(500).json({ 
-            message: 'Error registering user', 
+        return res.status(500).json({
+            message: 'Error registering user',
             error,
             isSuccess: false
+        })
+    }
+}
+
+export async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({email});
+        if (!user) {
+            res.status(401).json({
+                message: "Indentifiants incorrects"
+            })
+        }
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+        if (!passwordMatch) {
+            res.status(401).json({
+                message: "Indentifiants incorrects"
+            })
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_KEY, {
+            expiresIn: '1h',
+        });
+        res.status(200).json({
+            user,
+            token
+        })
+    } catch (error) {
+        res.status(500).json({
+            message:"Une erreur s'est produite",
+            error
         })
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { register } from '../controller/userController.js';
+import { login, register } from '../controller/userController.js';
 const userRoutes = express.Router();
 
 userRoutes.post('/register', register);
+userRoutes.post('/login', login);
 
 export default userRoutes;
