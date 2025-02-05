16282eb745644a37e276d532dab8b3a2f8b4232b
Nazaire02 <nazairendouffou@gmail.com>
2025-02-05 11:07:16 +0000

    get user cart

--- controller/cartController.js
+++ controller/cartController.js
@@ -2,13 +2,32 @@ import Cart from "../models/cart.js";
 
 export async function getAll(req, res) {
     try {
-        const carts = await Cart.find({items: {$exists: true, $not: { $size: 0 }}})
+        //get only carts which have items
+        const carts = await Cart.find({items: {$exists: true, $not: { $size: 0 }}}) 
                                 .populate("user")
                                 .populate("items.product");
 
         return res.status(200).json({
             isSucces: true,
             carts,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            isSucces: false,
+            message: "Error getting carts"
+        });
+    }
+}
+
+export async function getCartByUser(req, res) {
+    try {
+        const cart = await Cart.findOne({user: req.params.userId}) 
+                                .populate("user")
+                                .populate("items.product");
+
+        return res.status(200).json({
+            isSucces: true,
+            cart,
         });
     } catch (error) {
         return res.status(500).json({
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import verifyToken from "../middlewares/verifyToken.js";
 import { add, getOne } from "../controller/categoryController.js";
-import { getAll } from "../controller/cartController.js";
+import { getAll, getCartByUser } from "../controller/cartController.js";
 
 const cartRoutes = express.Router();
 
 cartRoutes.get("/get-all", verifyToken, getAll);
-cartRoutes.get("/get-user-cart/:userId", verifyToken, getOne);
+cartRoutes.get("/get-user-cart/:userId", verifyToken, getCartByUser);
 cartRoutes.patch("/update-items/:userId", verifyToken, add);
 
 export default cartRoutes;
